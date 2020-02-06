c5fdf9ae9c0b7059f474e14df4b5178b901fdfa6
Ben Hung <hben8352@gmail.com>
2020-02-05 21:32:58 -0500

    Update Two Sum.py
    map

--- Python/Two Sum.py
+++ Python/Two Sum.py
@@ -2,7 +2,7 @@
 # You may assume that each input would have exactly one solution, and you may not use the same element twice.
 # Given nums = [2, 7, 11, 15], target = 9, return [0, 1].
 
-# Two for loop
+# Two for loops
 #class Solution:
 #    def twoSum(self, nums: List[int], target: int) -> List[int]:
 #        res = {}
@@ -18,16 +18,28 @@
 #        return result
 
 # Sort
+#class Solution:
+#    def twoSum(self, nums: List[int], target: int) -> List[int]:
+#        res = {}
+#        result = []
+#        i = 0
+#        while i < len(nums):
+#            j = i + 1
+#            while j < len(nums):
+#                if nums[i] + nums[j] == target:
+#                    result = [i,j]
+#                j += 1
+#            i += 1
+#        return result
+
+# Map
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         res = {}
         result = []
-        i = 0
-        while i < len(nums):
-            j = i + 1
-            while j < len(nums):
-                if nums[i] + nums[j] == target:
-                    result = [i,j]
-                j += 1
-            i += 1
+        for key, value in enumerate(nums):
+            if target - value in res:
+                result.extend([res[target - value], key])
+            else:
+                res[value] = key
         return result
