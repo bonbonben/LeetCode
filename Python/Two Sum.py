6797420786d70309d87ab5b818dbe7e122914102
Ben Hung <hben8352@gmail.com>
2020-02-05 20:44:44 -0500

    Create Two Sum.py
    two for loops

---
+++ Python/Two Sum.py
@@ -0,0 +1,17 @@
+# Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# Given nums = [2, 7, 11, 15], target = 9, return [0, 1].
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        res = {}
+        result = []
+        i = 0
+        while i < len(nums):
+            j = i + 1
+            while j < len(nums):
+                if nums[i] + nums[j] == target:
+                    result = [i,j]
+                j += 1
+            i += 1
+        return result
