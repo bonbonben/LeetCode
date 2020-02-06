dbfc6370ce59edbed10f416e83b762f998d5e683
Ben Hung <hben8352@gmail.com>
2020-02-05 20:59:12 -0500

    Update Two Sum.py
    sort

--- Python/Two Sum.py
+++ Python/Two Sum.py
@@ -2,6 +2,22 @@
 # You may assume that each input would have exactly one solution, and you may not use the same element twice.
 # Given nums = [2, 7, 11, 15], target = 9, return [0, 1].
 
+# Two for loop
+#class Solution:
+#    def twoSum(self, nums: List[int], target: int) -> List[int]:
+#        res = {}
+#        result = []
+#        i = 0
+#        while i < len(nums):
+#            j = i + 1
+#            while j < len(nums):
+#                if nums[i] + nums[j] == target:
+#                    result = [i,j]
+#                j += 1
+#            i += 1
+#        return result
+
+# Sort
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         res = {}
