5b81c2d30bbe423e59a4dcd26615e5f00fb1313c
Ben Hung <hben8352@gmail.com>
2020-02-07 18:51:18 -0500

    Create Rotate Image.py

---
+++ Python/Rotate Image.py
@@ -0,0 +1,32 @@
+# You are given an n x n 2D matrix representing an image.
+# Rotate the image by 90 degrees (clockwise).
+
+# Given input matrix =
+# [
+#   [ 5, 1, 9,11],
+#   [ 2, 4, 8,10],
+#   [13, 3, 6, 7],
+#   [15,14,12,16]
+# ], 
+# rotate the input matrix in-place such that it becomes:
+#[
+#   [15,13, 2, 5],
+#   [14, 3, 4, 1],
+#   [12, 6, 8, 9],
+#   [16, 7,10,11]
+# ]
+
+# time complexity: O(m * n)
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix[0])
+        for i in range(0, n // 2):  # for boundaries
+            for j in range(i, n - i - 1):   # for swaps
+                tmp = matrix[n - 1 - j][i]
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1]
+                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 - i]
+                matrix[j][n - 1 - i] = matrix[i][j]
+                matrix[i][j] = tmp
