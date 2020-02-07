ffa265e2f43142c64caef10f46dc735022b76c88
Ben Hung <hben8352@gmail.com>
2020-02-06 22:01:11 -0500

    Update Group Anagrams.py

--- Python/Group Anagrams.py
+++ Python/Group Anagrams.py
@@ -2,9 +2,21 @@
 # Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
 # Output: [["eat","tea","ate"],["tan","nat"],["bat"]]
 
+# Sort
+#class Solution:
+#    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+#        ans = collections.defaultdict(list)
+#        for s in strs:
+#            ans[tuple(sorted(s))].append(s)
+#        return ans.values()
+
+# Count Map
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         ans = collections.defaultdict(list)
         for s in strs:
-            ans[tuple(sorted(s))].append(s)
+            count = [0]*26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            ans[tuple(count)].append(s)
         return ans.values()
