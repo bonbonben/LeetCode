9e0540530fc796ace32ee683495f6fadfc50a4d2
Ben Hung <hben8352@gmail.com>
2020-02-06 22:40:22 -0500

    Update Valid Anagram.py

--- Python/Valid Anagram.py
+++ Python/Valid Anagram.py
@@ -3,8 +3,21 @@
 # Output: true
 
 # Sort
+#class Solution:
+#    def isAnagram(self, s: str, t: str) -> bool:
+#        if(len(s) != len(t)):
+#            return false
+#        return sorted(s) == sorted(t)
+
+#Dict
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if(len(s) != len(t)):
-            return false
-        return sorted(s) == sorted(t)
+            return False
+        s_dict = {}
+        t_dict = {}
+        for ch in s:
+            s_dict[ch] = 1 if ch not in s_dict else s_dict[ch] + 1
+        for ch in t:
+            t_dict[ch] = 1 if ch not in t_dict else t_dict[ch] + 1
+        return s_dict == t_dict
