9b43b88a059790becbe21a190cc69f9d396769ce
Ben Hung <hben8352@gmail.com>
2020-02-14 20:00:28 -0500

    Update Valid Palindrome.py

--- Python/Valid Palindrome.py
+++ Python/Valid Palindrome.py
@@ -1,3 +1,12 @@
+# Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+# Note: For the purpose of this problem, we define empty string as valid palindrome.
+
+# Input: "A man, a plan, a canal: Panama"
+# Output: true
+
+# Input: "race a car"
+# Output: false
+
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         # isalnum() checks whether all the characters is a letter or a number
