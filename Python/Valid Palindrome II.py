7babc96214d9e2eebcbc8e02bf0906ff3a33a2aa
Ben Hung <hben8352@gmail.com>
2020-02-14 20:49:47 -0500

    Create Valid Palindrome II.py

---
+++ Python/Valid Palindrome II.py
@@ -0,0 +1,24 @@
+# Given a non-empty string s, you may delete at most one character. Judge whether you can make it a palindrome.
+
+# Input: "aba"
+# Output: True
+
+# Input: "abca"
+# Output: True
+# Explanation: You could delete the character 'c'.
+
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        return self.validPalindromeHelper(s, 0, len(s) - 1, 1)
+
+    def validPalindromeHelper(self, s, left, right, budget):
+        # budget can be more than 1
+        while right >= 0 and left <= right and s[left] == s[right]:
+            left += 1
+            right -= 1
+        if right < 0 or left >= right:
+            return True
+        if budget == 0:
+            return False
+        budget -= 1
+        return self.validPalindromeHelper(s, left + 1, right, budget) or self.validPalindromeHelper(s, left, right - 1, budget)
