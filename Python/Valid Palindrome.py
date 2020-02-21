4a97e619286235d377dc405005215401a2b32d57
Ben Hung <hben8352@gmail.com>
2020-02-20 20:59:19 -0500

    Update Valid Palindrome.py

--- Python/Valid Palindrome.py
+++ Python/Valid Palindrome.py
@@ -7,15 +7,31 @@
 # Input: "race a car"
 # Output: false
 
+#class Solution:
+#    def isPalindrome(self, s: str) -> bool:
+#        # isalnum() checks whether all the characters is a letter or a number
+#        alnum_s = [t.lower() for t in s if t.isalnum()]
+#        n = len(alnum_s)
+#        if n <= 1:
+#            return True
+#        mid = int(n / 2)
+#        for i in range(mid):
+#            if alnum_s[i] != alnum_s[n - 1 - i]:
+#                return False
+#        return True
+
+# time complexity: O(n).
+# space complexity: O(1).
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        # isalnum() checks whether all the characters is a letter or a number
-        alnum_s = [t.lower() for t in s if t.isalnum()]
-        n = len(alnum_s)
-        if n <= 1:
-            return True
-        mid = int(n / 2)
-        for i in range(mid):
-            if alnum_s[i] != alnum_s[n - 1 - i]:
+        s = ''.join(filter(str.isalnum, s))
+        left = 0
+        right = len(s) - 1
+        s = s.lower()
+        #print("s = ", s)
+        while left < right:
+            if(s[left] != s[right]):
                 return False
+            left += 1
+            right -= 1
         return True
