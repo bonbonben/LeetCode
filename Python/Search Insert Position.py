1f70662b705e30d96749eca0a0fd9f4ad3904632
Ben Hung <hben8352@gmail.com>
2020-02-27 12:21:38 -0500

    Create Search Insert Position.py

---
+++ Python/Search Insert Position.py
@@ -0,0 +1,28 @@
+# Given a sorted array and a target value, return the index if the target is found.
+# If not, return the index where it would be if it were inserted in order.
+# You may assume no duplicates in the array.
+
+# Input: [1,3,5,6], 5
+# Output: 2
+
+# Input: [1,3,5,6], 2
+# Output: 1
+
+# Input: [1,3,5,6], 7
+# Output: 4
+
+# Input: [1,3,5,6], 0
+# Output: 0
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l < r:
+            mid = int((l + r) / 2)
+            if nums[mid] < target:
+                l = mid + 1
+            else:
+                r = mid
+        if nums[l] < target:
+            return l + 1
+        return l
